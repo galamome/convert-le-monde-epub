d21de3008af5c4df19e1dc1cb8442715c4ee909b
Guillaume ALAMOME <guillaume.alamome@airbus.com>
2023-03-12 18:47:12 +0100

    Fonctionne

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # convert-le-monde-epub
 
+## Créer un virtualEnv
+
+## Create a new virtualenv named "convertlemonde"
+
+### Python 3.3+
+
+```python
+python3 -m venv venv
+```
+
+### Activate the virtualenv (OS X & Linux)
+
+```
+source project_env/bin/activate
+```
+
+### Geler la configuration
+
+```
+pip freeze > requirements.txt
+```
+
+### Charger une configuration existente
+
+```
+pip install -r requirements.txt
+```
+
 pandoc -f html -t epub3 --epub-metadata=metadata.xml -o output.epub input.html
 
 
--- retrieve_articles.py
+++ retrieve_articles.py
@@ -3,99 +3,124 @@ import shutil
 import tempfile
 import urllib.request
 from lxml import etree
+import asyncio
+import os.path
+import aiofiles
 
-urlToRetrieve = "https://www.lemonde.fr/international/article/2023/02/13/alexei-venediktov-la-haine-est-entree-au-sein-de-chaque-famille-russe_6161602_3210.html"
-cookie = "lmd_gdpr_token=1; atauthority=%7B%22name%22%3A%22atauthority%22%2C%22val%22%3A%7B%22authority_name%22%3A%22cnil%22%2C%22visitor_mode%22%3A%22exempt%22%7D%2C%22options%22%3A%7B%22end%22%3A%222024-04-12T17%3A11%3A46.983Z%22%2C%22path%22%3A%22%2F%22%7D%7D; atidx=20E50FC4-85A0-49A7-B48F-AC3A21509C5F; atid=20E50FC4-85A0-49A7-B48F-AC3A21509C5F; lmd_sso_twipe=%7B%22token%22%3A%22SrWrUiyFGMNI4QIoA49%5C%2FxiAOOuh06HZLaDsvqrJiujE%3D%22%7D; lmd_a_s=2XZdAdv51r0S15vcazvfPrByZ7GHggmU1Xlog4os0GwLeh2cXvmhHG3uFKoWf2AE; lmd_a_ld=cq%2FED61uukL7lcpak8TTaiZs9zIClG4B15G0HFJLvg0%3D; lmd_a_sp=2XZdAdv51r0S15vcazvfPrByZ7GHggmU1Xlog4os0GwLeh2cXvmhHG3uFKoWf2AE; lmd_stay_connected=0; lmd_a_m=SrWrUiyFGMNI4QIoA49%2FxiAOOuh06HZLaDsvqrJiujE%3D; euconsent-v2=CPdnuOJPdnuOJFzABCFRBdCgAAAAAAAAAAAAAAAAAAAA; lmd_consent=%7B%22userId%22%3A%228ce667c9-04da-4f5f-a51a-ca901765e8c2%22%2C%22timestamp%22%3A%221660307956.152987654%22%2C%22version%22%3A1%2C%22cmpId%22%3A371%2C%22displayMode%22%3A%22subscriber%22%2C%22purposes%22%3A%7B%22analytics%22%3Afalse%2C%22ads%22%3Afalse%2C%22personalization%22%3Afalse%2C%22mediaPlatforms%22%3Afalse%2C%22social%22%3Afalse%7D%7D; lmd_cap=_q7qieqqqk; lmd_ab=hHupglDtCPSH8wIk3cUj8EEik%2F97FceyGGTP%2FghGCGZQyt9yg6LoPbrWWvbw9SxAHnRJL12fmdsTe%2BGZUJjy9Pj8id0T6pc6m9JZNoeb5GY%2BJ1ffOwBBW8McaVQPwE86RQvFxMyueC4GSxFUMXwrh7FM4TWSSOwS0v0ZqaQGxrH%2FH4oUw2spHH9svjfNQheM80VUlKIaGvE8q1TmHA%3D%3D; lmd_a_c=1"
-splittedUrl = urlToRetrieve.split('/')
+######################################################
+# Constants
+######################################################
 
-lastIndex = len(splittedUrl) - 1
+TO_REMOVE_XPATHS = ['//script',
+                    '//noscript',
+                    '//link',
+                    "//section[@class='catcher catcher--inline']",
+                    "//section[@class='article__reactions']",
+                    "//section[@class='inread js-services-inread']",
+                    "//footer[@class='article__footer-single old__article-footer']",
+                    "//aside[@class='aside__iso   old__aside']",
+                    "//footer[@class='footer footer--abo old__footer']",
+                    "//section[@id='js-capping']",
+                    "//section[@id='js-capping-old-article']",
+                    "//div[@class='Header__nav-container']",
+                    "//div[@id='Header']",
+                    "//div[@id='cover']",
+                    "//div[@id='dfp-habillage']",
+                    "//div[@id='banniere_haute']",
+                    "//ul[@class='meta meta__social   old__meta-social meta__social--gift']",
+                    "//section[@id='js-message-register']",
+                    "//div[@class='lazy-bizdev']",
+                    "//div[@id='overlay']",
+                    "//span[@class='sr-only']",
+                    "//div[@class='lmd-dropdown__overlay']",
+                    "//ul[@class='meta meta__social  meta__social--opinion old__meta-social meta__social--gift']"
+                    #,"//span[@class='meta__article-en-fr-url-link'", "//span[@class='icon__premium'"
+                    ]
 
-fileName = f'{splittedUrl[lastIndex-3]}_{splittedUrl[lastIndex-2]}_{splittedUrl[lastIndex-1]}_{splittedUrl[lastIndex]}'
+########################################################################
+# Functions
+########################################################################
 
-a_request = urllib.request.Request(urlToRetrieve)
+def remove_unnecessary_elements(xml_tree, to_be_removed_xpaths):
+    # Loop through of XPath to be removed
+    for xpath in to_be_removed_xpaths:
+        # Remove particular elements
+        for s in xml_tree.xpath(xpath):
+            s.getparent().remove(s)
+    return xml_tree
 
-a_request.add_header("Cookie", cookie)
-with urllib.request.urlopen(a_request) as response:
-    content_char_set = response.headers.get_content_charset()
-    the_page =  response.read().decode(response.headers.get_content_charset())
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
 
-    # Replace attribute not allowed
-    # Attribute name "crossorigin" associated with an element type "link" must be followed by the ' = ' character. 
-    modified = the_page.replace(" crossorigin>", " crossorigin=\"anonymous\">")
+    stdout, stderr = await proc.communicate()
 
-    parser = etree.HTMLParser()
-    tree = etree.parse(io.StringIO(modified), parser)
-
-    # Remove <script elements
-    for s in tree.xpath('//script'):
-        s.getparent().remove(s)
-    for s in tree.xpath('//link'):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//section[@class='catcher catcher--inline']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//section[@class='article__reactions']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//section[@class='inread js-services-inread']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//footer[@class='article__footer-single old__article-footer']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//aside[@class='aside__iso   old__aside']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//footer[@class='footer footer--abo old__footer']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//section[@id='js-capping']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//section[@id='js-capping-old-article']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//div[@class='Header__nav-container']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//div[@id='Header']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//div[@id='cover']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//div[@id='dfp-habillage']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//div[@id='banniere_haute']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//ul[@class='meta meta__social   old__meta-social meta__social--gift']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//section[@id='js-message-register']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//div[@class='lazy-bizdev']"):
-        s.getparent().remove(s)
-    # Remove particular elements
-    for s in tree.xpath("//div[@id='overlay']"):
-        s.getparent().remove(s)
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode()}')
+    if stderr:
+        print(f'[stderr]\n{stderr.decode()}')
 
 
-    without_script = etree.tostring(tree.getroot(), encoding='UTF-8')
+async def get_and_convert_le_monde_article(articleUrl, cookie):
+    splittedUrl = articleUrl.split('/')
 
-    text_file = open(fileName, "wb")
-    n = text_file.write(without_script)
-    text_file.close()
+    lastIndex = len(splittedUrl) - 1
+    htmlFileName = f'{splittedUrl[lastIndex-3]}_{splittedUrl[lastIndex-2]}_{splittedUrl[lastIndex-1]}_{splittedUrl[lastIndex]}'
+    nakedFileName = htmlFileName.split('.')[0]
+    a_request = urllib.request.Request(articleUrl)
 
-    # process Unicode text
-    #with io.open(fileName,'w',encoding='utf8') as f:
-     #   f.write(without_script)
+    a_request.add_header("Cookie", cookie)
+    with urllib.request.urlopen(a_request) as response:
+        content_char_set = response.headers.get_content_charset()
+        the_page =  response.read().decode(response.headers.get_content_charset())
 
+        # Replace attribute not allowed
+        # Attribute name "crossorigin" associated with an element type "link" must be followed by the ' = ' character. 
+        modified = the_page.replace(" crossorigin>", " crossorigin=\"anonymous\">")
 
+        parser = etree.HTMLParser()
+        tree = etree.parse(io.StringIO(modified), parser)
 
-# A prendre
-#  <article class="article__content old__article-content-single">
-# //article[@class='article__content old__article-content-single']
+        # Remove all unnecessary elements from the HTML, based on XPath
+        tree = remove_unnecessary_elements(tree, TO_REMOVE_XPATHS)
+
+        without_script = etree.tostring(tree.getroot(), encoding='UTF-8')
+
+        # Write file
+        if not os.path.exists(htmlFileName):
+            async with aiofiles.open(htmlFileName, "wb") as text_file:
+                await text_file.write(without_script)
+                return nakedFileName
+        else:
+            print(f'File {htmlFileName} already exists, and will NOT be written')
+    return ""
+
+def convert(nakedFileName):
+    # Write file
+    epubFileName = f'{nakedFileName}.epub'
+    htmlFileName = f'{nakedFileName}.html'
+    if not os.path.isfile(epubFileName):
+        # Prepare the command to run "pandoc"
+        command_to_convert_to_epub = f'pandoc -f html -t epub2 -o {epubFileName} {htmlFileName}'
+
+        return command_to_convert_to_epub
+    else:
+        print(f'File {epubFileName} already exists, and will NOT be created')
+    return ""
+
+##########################################
+
+#urlToRetrieve = "https://www.lemonde.fr/international/article/2023/02/13/alexei-venediktov-la-haine-est-entree-au-sein-de-chaque-famille-russe_6161602_3210.html"
+myCookie = "lmd_gdpr_token=1; atauthority=%7B%22name%22%3A%22atauthority%22%2C%22val%22%3A%7B%22authority_name%22%3A%22cnil%22%2C%22visitor_mode%22%3A%22exempt%22%7D%2C%22options%22%3A%7B%22end%22%3A%222024-04-19T10%3A01%3A22.394Z%22%2C%22path%22%3A%22%2F%22%7D%7D; atidx=20E50FC4-85A0-49A7-B48F-AC3A21509C5F; atid=20E50FC4-85A0-49A7-B48F-AC3A21509C5F; lmd_sso_twipe=%7B%22token%22%3A%22SrWrUiyFGMNI4QIoA49%5C%2FxiAOOuh06HZLaDsvqrJiujE%3D%22%7D; lmd_a_s=2XZdAdv51r0S15vcazvfPrByZ7GHggmU1Xlog4os0GwLeh2cXvmhHG3uFKoWf2AE; lmd_a_ld=cq%2FED61uukL7lcpak8TTaiZs9zIClG4B15G0HFJLvg0%3D; lmd_a_sp=2XZdAdv51r0S15vcazvfPrByZ7GHggmU1Xlog4os0GwLeh2cXvmhHG3uFKoWf2AE; lmd_stay_connected=0; lmd_a_m=SrWrUiyFGMNI4QIoA49%2FxiAOOuh06HZLaDsvqrJiujE%3D; euconsent-v2=CPdnuOJPdnuOJFzABCFRBdCgAAAAAAAAAAAAAAAAAAAA; lmd_consent=%7B%22userId%22%3A%228ce667c9-04da-4f5f-a51a-ca901765e8c2%22%2C%22timestamp%22%3A%221660307956.152987654%22%2C%22version%22%3A1%2C%22cmpId%22%3A371%2C%22displayMode%22%3A%22subscriber%22%2C%22purposes%22%3A%7B%22analytics%22%3Afalse%2C%22ads%22%3Afalse%2C%22personalization%22%3Afalse%2C%22mediaPlatforms%22%3Afalse%2C%22social%22%3Afalse%7D%7D; lmd_cap=_q7qieqqqk; lmd_a_c=1; lmd_ab=UgbDLT9lDzbt5hZhsq%2BccSlNC3R98yLL7kEM78XDi7ZuCASPgLzk%2BxHK6Le%2FioBaGc63m%2FA9KnW%2FsbBlQhg6FP8RdtTU3l29f5TYaqX%2B5vRWJ4pqo1ZuoF4iDgrNbrnji1UH%2BUSleY2oBP09ZVqbugxvUSYt4yfJkfOfAOU%3D"
+
+f = open("Articles_fevrier.txt", "r")
+for line in f.readlines():
+    if "https://www.lemonde.fr/" in line:
+        asyncio.run(get_and_convert_le_monde_article(line, myCookie))
+        #command_to_convert_to_epub = convert(articleName)
+
+        # Run the command to run "pandoc"
+        #asyncio.run(run(command_to_convert_to_epub))
