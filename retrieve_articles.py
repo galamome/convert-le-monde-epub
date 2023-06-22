73e03f07c15e6ac2d10b7c66856d3a92d19d1fe1
Guillaume ALAMOME <guillaume.alamome@airbus.com>
2023-06-22 18:23:58 +0200

    Doc

--- retrieve_articles.py
+++ retrieve_articles.py
@@ -8,6 +8,7 @@ import asyncio
 import os.path
 import aiofiles
 import aiohttp
+import argparse
 
 
 ######################################################
@@ -47,6 +48,18 @@ TO_REMOVE_XPATHS = ['//title',
 ########################################################################
 # Functions
 ########################################################################
+
+def parse_args():
+    parser=argparse.ArgumentParser(description="A script to retrieve Le Monde articles")
+    parser.add_argument("--articleFile",
+                        type=str,
+                        default="articles.txt",
+                        help='fichier liste des articles')
+    parser.add_argument("--cookie",
+                        type=str,
+                        help='cookie doit être entouré de guillemets double')
+    args=parser.parse_args()
+    return args
 
 def remove_unnecessary_elements(xml_tree: etree._ElementTree, to_be_removed_xpaths: List[str]) -> etree._ElementTree:
     """
@@ -147,12 +160,14 @@ async def get_and_convert_le_monde_article(articleUrl, cookie):
         print(f'File {htmlFileName} already exists, and article {articleUrl} will NOT be written')
 
 ##########################################
-myCookie = "lmd_gdpr_token=1; atauthority=%7B%22name%22%3A%22atauthority%22%2C%22val%22%3A%7B%22authority_name%22%3A%22cnil%22%2C%22visitor_mode%22%3A%22exempt%22%7D%2C%22options%22%3A%7B%22end%22%3A%222024-05-05T16%3A01%3A21.098Z%22%2C%22path%22%3A%22%2F%22%7D%7D; atidx=20E50FC4-85A0-49A7-B48F-AC3A21509C5F; atid=20E50FC4-85A0-49A7-B48F-AC3A21509C5F; lmd_sso_twipe=%7B%22token%22%3A%22SrWrUiyFGMNI4QIoA49%5C%2FxiAOOuh06HZLaDsvqrJiujE%3D%22%7D; lmd_a_s=2XZdAdv51r0S15vcazvfPrByZ7GHggmU1Xlog4os0GwLeh2cXvmhHG3uFKoWf2AE; lmd_a_ld=cq%2FED61uukL7lcpak8TTaiZs9zIClG4B15G0HFJLvg0%3D; lmd_a_sp=2XZdAdv51r0S15vcazvfPrByZ7GHggmU1Xlog4os0GwLeh2cXvmhHG3uFKoWf2AE; lmd_stay_connected=0; lmd_a_m=SrWrUiyFGMNI4QIoA49%2FxiAOOuh06HZLaDsvqrJiujE%3D; euconsent-v2=CPdnuOJPdnuOJFzABCFRBdCgAAAAAAAAAAAAAAAAAAAA; lmd_consent=%7B%22userId%22%3A%228ce667c9-04da-4f5f-a51a-ca901765e8c2%22%2C%22timestamp%22%3A%221660307956.152987654%22%2C%22version%22%3A1%2C%22cmpId%22%3A371%2C%22displayMode%22%3A%22subscriber%22%2C%22purposes%22%3A%7B%22analytics%22%3Afalse%2C%22ads%22%3Afalse%2C%22personalization%22%3Afalse%2C%22mediaPlatforms%22%3Afalse%2C%22social%22%3Afalse%7D%7D; lmd_cap=_q7qieqqqk; lmd_a_c=1; lmd_ab=DLkCMS4CG7O9viiV6CjuiVvnjJKjO5%2BwRBR4jXrr9jk1RoxYo0OxeekkigAyYWYWWW5epxbZENt5IepVjDafcksNxsHrolpSauy%2FUzD1Nk%2Fl6lGfBB9CWYaiEksCamk7SO3tMim3bxneIANvS1vb%2FOkDYFlF6jinykqgAng0zCpZ80Jun27WsTqX%2Bt%2FDuNlpl%2FGlF8kob6j0D8A%3D"
-
-
-
 async def main():
-    f = open("Articles_jusqua_7_avril.txt", "r")
+    # Retrieve args
+    inputs=parse_args()
+
+    articleFileName = inputs.articleFile
+    myCookie = inputs.cookie
+    print(myCookie)
+    f = open(articleFileName, "r")
     urls = set([])
     for line in f.readlines():
         if "https://www.lemonde.fr/" in line:
